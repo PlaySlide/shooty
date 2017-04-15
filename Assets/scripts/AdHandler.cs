c01a2f4c7927af81327ce4649279bb2d98ab3dda
Jason Surguine <jaybotx220@Jasons-MacBook-Pro.local>
2017-04-15 22:50:26 +0900

    first add

---
+++ Assets/scripts/AdHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdHandler : MonoBehaviour
+{
+    public static AdHandler adHandler;
+
+    public string iosId_UnityAds, androidId_UnityAds;
+
+    void Awake()
+    {
+        adHandler = this;
+
+#if UNITY_ANDROID
+        Advertisement.Initialize(androidId_UnityAds);
+#elif UNITY_IPHONE
+        Advertisement.Initialize(iosId_UnityAds);
+#endif
+    }
+
+    public bool ShowRewardedVideoAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                break;
+        }
+    }
+}
---
+++ Assets/scripts/getShootAngle.js
@@ -0,0 +1,74 @@
+﻿import HutongGames.PlayMaker;
+
+//var globalVariables = FsmVariables.GlobalVariables;
+//var shootZ = globalVariables.GetFsmFloat ("shootZ");
+//var incomingPowerX = globalVariables.GetFsmFloat ("shootX");
+//var incomingPowerY = globalVariables.GetFsmFloat ("shootY");
+
+//var incomingAngle = globalAngle;
+//var incomingAngle = shootZ;
+var outgoingPowerX = 0.0;
+var outgoingPowerY = 90.0;
+
+function getShootAngle(incomingAngle : float){
+	if (incomingAngle < 0){
+		incomingAngle = incomingAngle + 360;
+		} 
+
+	if (incomingAngle == 0) {
+		outgoingPowerX = -90.0;
+		outgoingPowerY = 0.0;
+	}
+
+	if (incomingAngle >= 1.0 && incomingAngle <= 89.0) {
+		outgoingPowerX = -90.0;
+		outgoingPowerY = 0.0;
+		for (var i = 0.0; i < incomingAngle; i++){
+			outgoingPowerX++; 
+			outgoingPowerY--;
+		}
+	}
+			// 90
+	if (incomingAngle == 90.0) {
+		outgoingPowerX = 90.0;
+		outgoingPowerY = 0.0;
+	}
+
+	if (incomingAngle >= 91.0 && incomingAngle <= 179.0) {
+		outgoingPowerX = 90.0;
+		outgoingPowerY = 0.0;
+		for (var j = 0.0; j < (180 - incomingAngle); j++){
+			outgoingPowerX--; 
+			outgoingPowerY--;
+		}
+	}
+
+	// 180	
+	if (incomingAngle == 180.0) {
+		outgoingPowerX = 90.0;
+		outgoingPowerY = 0.0;
+	}
+	if (incomingAngle >= 181.0 && incomingAngle <= 269.0) {
+		outgoingPowerX = 90.0;
+		outgoingPowerY = 0.0;
+		for (var k = 0.0; k < (270.0 - incomingAngle); k++){
+			outgoingPowerX--; 
+			outgoingPowerY++;
+		}
+	} 	
+
+	//270
+	if (incomingAngle == 270.0) {
+		outgoingPowerX = -90.0;
+		outgoingPowerY = 0.0;	
+		}
+
+	if (incomingAngle >= 271.0 && incomingAngle <= 359.0) {
+		outgoingPowerX = -90.0;
+		outgoingPowerY = 0.0;
+		for (var m = 0; m < (360.0 - incomingAngle); m++){
+			outgoingPowerX++; 
+			outgoingPowerY++;
+		}
+	}
+}
